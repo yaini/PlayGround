ca20f6650aae5c5605eed1cb17b3e146eb8315ab
yaini <ssafy.soyeon@gmail.com>
2022-03-03 00:04:43 +0900

    SPRING-2 creational: prototype

--- design-pattern/src/main/java/creational/Prototype.java
+++ design-pattern/src/main/java/creational/Prototype.java
@@ -1,7 +1,50 @@
 package creational;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Prototype {
     public static void main(String[] args){
+        MoneyFactory moneyFactory = new MoneyFactory(new Coin(100));
 
+        List<Money> wallet = new ArrayList<>();
+        for( int i=0; i<10; i++ ){
+            wallet.add(moneyFactory.makeMoney());
+        }
     }
+}
+
+class Money implements Cloneable{
+    int price;
+
+    public Object clone() {
+        try {
+            Money copy = (Money) super.clone();
+            return copy;
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
+
+class Coin extends Money{
+
+    Coin(int price){
+        this.price = price;
+    }
+}
+
+class MoneyFactory{
+
+    private Money money;
+
+    public MoneyFactory(Money money){
+        this.money = money;
+    }
+
+    public Money makeMoney(){
+        return (Money) money.clone();
+    }
+
 }
