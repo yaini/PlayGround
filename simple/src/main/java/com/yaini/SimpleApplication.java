c1a6f787c29d06907476db14062ad6ca30f7494e
yaini <ssafy.soyeon@gmail.com>
2022-03-24 22:14:54 +0900

    SPRING-7 feat: controller

---
+++ simple/src/main/java/com/yaini/SimpleApplication.java
@@ -0,0 +1,11 @@
+package com.yaini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleApplication.class, args);
+    }
+}
---
+++ simple/src/main/java/com/yaini/controller/SimpleController.java
@@ -0,0 +1,13 @@
+package com.yaini.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SimpleController {
+
+    @GetMapping("/hello")
+    public String simple() {
+        return "Hello World!";
+    }
+}
