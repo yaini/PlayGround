57051ebb87b2a36ab7863f74f61d36980462ef7d
yaini <ssafy.soyeon@gmail.com>
2022-05-02 00:19:59 +0900

    SPRING-13 fix: chore

--- simple/deploy/mysql-init.d/init.sql
+++ simple/deploy/mysql-init.d/init.sql
@@ -4,4 +4,4 @@ CREATE USER 'simple'@'%' IDENTIFIED BY 'simple';
 GRANT ALL PRIVILEGES ON *.* TO 'simple'@'localhost';
 GRANT ALL PRIVILEGES ON *.* TO 'simple'@'%';
 
-CREATE DATABASE SIMPLE;
+CREATE DATABASE simple DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
