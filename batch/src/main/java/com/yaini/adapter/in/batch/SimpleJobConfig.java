109d2adee8a87be207d66eb90e6f12392079d981
yaini <ssafy.soyeon@gmail.com>
2022-11-03 23:04:36 +0900

    SPRING-23 feat: simpleJob

---
+++ batch/src/main/java/com/yaini/adapter/in/batch/SimpleJobConfig.java
@@ -0,0 +1,44 @@
+package com.yaini.adapter.in.batch;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@RequiredArgsConstructor
+@Configuration
+public class SimpleJobConfig {
+
+	private final JobBuilderFactory jobBuilderFactory;
+	private final StepBuilderFactory stepBuilderFactory;
+
+	@Bean
+	public Job batchJob(){
+
+		return jobBuilderFactory.get("batchJob")
+				.start(startStep())
+				.next(nextStep())
+				.build();
+	}
+
+	@Bean
+	public Step startStep(){
+
+		return stepBuilderFactory.get("startStep")
+				.tasklet( ((contribution, chunkContext) -> RepeatStatus.FINISHED))
+				.build();
+	}
+
+	@Bean
+	public Step nextStep(){
+
+		return stepBuilderFactory.get("nextStep")
+				.tasklet( ((contribution, chunkContext) -> RepeatStatus.FINISHED))
+				.build();
+	}
+
+}
