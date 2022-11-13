522430ddd5d2b95524dad8646062718115e3216e
yaini <ssafy.soyeon@gmail.com>
2022-11-14 01:35:19 +0900

    SPRING-23 feat: JdbcCursorItemReader

---
+++ batch/src/main/java/com/yaini/adapter/in/batch/JdbcCursorConfig.java
@@ -0,0 +1,53 @@
+package com.yaini.adapter.in.batch;
+
+import com.yaini.adapter.in.batch.item.CustomerItem;
+import javax.sql.DataSource;
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@RequiredArgsConstructor
+@Configuration
+public class JdbcCursorConfig {
+
+  public static final String JOB_NAME = "JDBC_CURSOR_JOB";
+  public static final int CHUNK_SIZE = 100;
+
+  private final JobBuilderFactory jobBuilderFactory;
+  private final StepBuilderFactory stepBuilderFactory;
+  private final DataSource dataSource;
+
+  @Bean(JOB_NAME)
+  public Job batchJob() {
+
+    return jobBuilderFactory.get(JOB_NAME).start(jdbcStep()).build();
+  }
+
+  @Bean
+  public Step jdbcStep() {
+
+    return stepBuilderFactory
+        .get("jdbcStep")
+        .chunk(CHUNK_SIZE)
+        .reader(jdbcCursorItemReader())
+        .build();
+  }
+
+  @Bean
+  public ItemReader<CustomerItem> jdbcCursorItemReader() {
+
+    return new JdbcCursorItemReaderBuilder<CustomerItem>()
+        .name("jdbcCursorItemReader")
+        .fetchSize(CHUNK_SIZE)
+        .sql("select id, name, date from customer")
+        .beanRowMapper(CustomerItem.class)
+        .dataSource(this.dataSource)
+        .build();
+  }
+}
--- batch/src/main/java/com/yaini/adapter/in/batch/PartitionJobConfig.java
+++ batch/src/main/java/com/yaini/adapter/in/batch/PartitionJobConfig.java
@@ -1,23 +1,27 @@
 package com.yaini.adapter.in.batch;
 
 import com.yaini.adapter.in.batch.item.CustomerItem;
+import com.yaini.adapter.in.batch.partitioner.ColumnRangePartitioner;
 import java.util.HashMap;
 import java.util.Map;
 import javax.sql.DataSource;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
-import org.springframework.batch.core.step.tasklet.TaskletStep;
+import org.springframework.batch.core.partition.support.Partitioner;
 import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
 import org.springframework.batch.item.database.JdbcBatchItemWriter;
 import org.springframework.batch.item.database.JdbcPagingItemReader;
 import org.springframework.batch.item.database.Order;
 import org.springframework.batch.item.database.support.MySqlPagingQueryProvider;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.core.ColumnMapRowMapper;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
 
 @RequiredArgsConstructor
 @Configuration
@@ -35,21 +39,59 @@ public class PartitionJobConfig {
     return jobBuilderFactory
         .get(JOB_NAME)
         .incrementer(new RunIdIncrementer())
-        .start(new TaskletStep())
+        .start(masterStep())
         .build();
   }
 
   @Bean
-  public JdbcPagingItemReader<CustomerItem> pagingItemReader() {
+  public Step masterStep() {
+
+    return stepBuilderFactory
+        .get("masterStep")
+        .partitioner(slaveStep().getName(), partitioner())
+        .step(slaveStep())
+        .gridSize(4)
+        .taskExecutor(new SimpleAsyncTaskExecutor())
+        .build();
+  }
+
+  @Bean
+  public Step slaveStep() {
+
+    return stepBuilderFactory
+        .get("slaveStep")
+        .<CustomerItem, CustomerItem>chunk(100)
+        .reader(pagingItemReader(null, null))
+        .writer(batchItemWriter())
+        .build();
+  }
+
+  @Bean
+  public Partitioner partitioner() {
+    ColumnRangePartitioner columnRangePartitioner = new ColumnRangePartitioner();
+
+    columnRangePartitioner.setDataSource(this.dataSource);
+    columnRangePartitioner.setColumn("id");
+    columnRangePartitioner.setTable("customer");
+
+    return columnRangePartitioner;
+  }
+
+  @Bean
+  @StepScope
+  public JdbcPagingItemReader<CustomerItem> pagingItemReader(
+      @Value("#{stepExecutionContext['minValue']}") Long minValue,
+      @Value("#{stepExecutionContext['maxValue']}") Long maxValue) {
     JdbcPagingItemReader<CustomerItem> reader = new JdbcPagingItemReader<>();
 
     reader.setDataSource(this.dataSource);
     reader.setFetchSize(1000);
-    reader.setRowMapper(new ColumnMapRowMapper());
+    reader.setRowMapper(new ColumnRowMapper());
 
     MySqlPagingQueryProvider provider = new MySqlPagingQueryProvider();
     provider.setSelectClause("id, name, birth");
     provider.setFromClause("from customer");
+    provider.setWhereClause("where id >= " + minValue + "and id < " + maxValue);
 
     Map<String, Order> sortKey = new HashMap<>(1);
     sortKey.put("id", Order.ASCENDING);
