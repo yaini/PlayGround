53a7d86b2e958b8ad9e84b4084f36b6c192641ad
yaini <ssafy.soyeon@gmail.com>
2022-11-20 03:07:43 +0900

    SPRING-23 feat: StaxEventItemReader

---
+++ batch/src/main/java/com/yaini/adapter/in/batch/XMLStaxEventJobConfig.java
@@ -0,0 +1,81 @@
+package com.yaini.adapter.in.batch;
+
+import com.yaini.adapter.in.batch.item.CustomerItem;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.xml.builder.StaxEventItemReaderBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.oxm.xstream.XStreamMarshaller;
+
+@Slf4j
+@RequiredArgsConstructor
+@Configuration
+public class XMLStaxEventJobConfig {
+
+  public static final String JOB_NAME = "XML_STAX_EVENT_JOB";
+  public static final String FILE_PATH = "./";
+  public static final String FILE_NAME = "customer.xml";
+  public static final int CHUNK_SIZE = 2;
+
+  private final JobBuilderFactory jobBuilderFactory;
+  private final StepBuilderFactory stepBuilderFactory;
+
+  @Bean(JOB_NAME)
+  public Job xmlStatEventJob() {
+
+    return jobBuilderFactory.get(JOB_NAME).start(xmlStaxEventStep()).build();
+  }
+
+  @Bean
+  public Step xmlStaxEventStep() {
+
+    return stepBuilderFactory
+        .get("xmlStaxEventStep")
+        .<CustomerItem, CustomerItem>chunk(CHUNK_SIZE)
+        .reader(xmlFileItemReader())
+        .writer(xmlFileItemWriter())
+        .build();
+  }
+
+  @Bean
+  public ItemReader<CustomerItem> xmlFileItemReader() {
+
+    return new StaxEventItemReaderBuilder<CustomerItem>()
+        .name("xmlFileItemReader")
+        .resource(new ClassPathResource(FILE_PATH + FILE_NAME))
+        .addFragmentRootElements("customer")
+        .unmarshaller(itemMarshaller())
+        .build();
+  }
+
+  @Bean
+  public XStreamMarshaller itemMarshaller() {
+    Map<String, Class<?>> aliases = new HashMap<>();
+    aliases.put("customer", CustomerItem.class);
+    aliases.put("id", Long.class);
+    aliases.put("name", String.class);
+    aliases.put("birth", LocalDateTime.class);
+
+    XStreamMarshaller marshaller = new XStreamMarshaller();
+    marshaller.setAliases(aliases);
+
+    return marshaller;
+  }
+
+  @Bean
+  public ItemWriter<CustomerItem> xmlFileItemWriter() {
+
+    return items -> items.forEach(item -> log.info("[xmlFileItemWriter] items :: {} ", item));
+  }
+}
---
+++ batch/src/test/java/com/yaini/job/XMLStaxEventJobTest.java
@@ -0,0 +1,45 @@
+package com.yaini.job;
+
+import com.yaini.adapter.in.batch.XMLStaxEventJobConfig;
+import java.time.LocalDateTime;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.batch.core.BatchStatus;
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.batch.test.context.SpringBatchTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@EnableAutoConfiguration
+@EnableBatchProcessing
+@SpringBatchTest
+@SpringBootTest(
+    classes = {XMLStaxEventJobConfig.class},
+    webEnvironment = SpringBootTest.WebEnvironment.NONE)
+public class XMLStaxEventJobTest {
+
+  @Autowired private JobLauncherTestUtils launcherUtils;
+
+  @Test
+  public void XMLStaxEvent_API를_통해_파일의_데이터를_읽고_쓸_수_있다() throws Exception {
+    // given
+    JobParameters jobParameters =
+        new JobParametersBuilder()
+            .addString("name", "xmlFileTest")
+            .addString("date", LocalDateTime.now().toString())
+            .toJobParameters();
+
+    // when
+    JobExecution execution = launcherUtils.launchJob(jobParameters);
+
+    // then
+    Assertions.assertEquals(execution.getStatus(), BatchStatus.COMPLETED);
+    Assertions.assertEquals(execution.getExitStatus(), ExitStatus.COMPLETED);
+  }
+}
