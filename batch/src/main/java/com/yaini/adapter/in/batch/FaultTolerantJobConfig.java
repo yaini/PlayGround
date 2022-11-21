7022e0fac969774d279bedd17924c48d48e33d2b
yaini <ssafy.soyeon@gmail.com>
2022-11-21 22:12:31 +0900

    SPRING-23 feat: Retry

--- batch/src/main/java/com/yaini/adapter/in/batch/FaultTolerantJobConfig.java
+++ batch/src/main/java/com/yaini/adapter/in/batch/FaultTolerantJobConfig.java
@@ -19,7 +19,7 @@ public class FaultTolerantJobConfig {
   public static final String JOB_NAME = "FAULT_TOLERANT_JOB";
   public static final int CHUNK_SIZE = 2;
   public static final int SKIP_LIMIT = 10;
-  public static final int RETRY_LIMIT = 10;
+  public static final int RETRY_LIMIT = 3;
 
   private final JobBuilderFactory jobBuilderFactory;
   private final StepBuilderFactory stepBuilderFactory;
@@ -39,6 +39,7 @@ public class FaultTolerantJobConfig {
         .reader(
             () -> {
               int item = new SplittableRandom().nextInt();
+              log.info("[ItemReader] item :: {} ", item);
               if (item % 2 == 0) {
                 return item;
               }
@@ -48,6 +49,7 @@ public class FaultTolerantJobConfig {
         .processor(
             (ItemProcessor<Integer, String>)
                 item -> {
+                  log.info("[ItemProcessor] item :: {} ", item);
                   if (item % 3 == 0) {
                     return Integer.toString(item);
                   }
